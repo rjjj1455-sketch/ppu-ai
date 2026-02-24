d1be7ff647db4c67e7207be5f718e37a1179f290
jova <rjjj1455@gmail.com>
2026-02-24 21:30:49 +0800

    revisi deploy login

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LayananPublikController;
 use App\Http\Controllers\Admin\ValidasiController;
